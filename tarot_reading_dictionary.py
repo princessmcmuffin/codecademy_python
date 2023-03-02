5bfa0ea6c320da937f9dec10a944b43adbe73a75
Steph <39493915+princessmcmuffin@users.noreply.github.com>
2023-03-02 19:16:51 +0000

    Updated annotations

--- tarot_reading_dictionary.py
+++ tarot_reading_dictionary.py
@@ -1,13 +1,13 @@
 tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
 
-#Set the table with an open spread
+#Set the reading table with a blank dictionary called 'spread'
 spread = {}
 
-#Draw 3 cards
+#Draw 3 cards. Remove them from the tarot deck and add them to your spread
 spread["past"] = tarot.pop(13)
 spread["present"] = tarot.pop(22)
 spread["future"] = tarot.pop(10)
 
-#Reveal your destiny
+#Reveal your destiny by reading what cards have been picked for each phase
 for key,value in spread.items():
   print("Your " + key + " is the " + str(value) + " card.")
